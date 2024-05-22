38e805596b9743315cb38f09b589ae0e0a016e97
bayram <bayramguney@yahoo.com>
2024-05-22 16:30:27 -0400

    Day1  completed

---
+++ Day1/3input_function.py
@@ -0,0 +1,10 @@
+# this is a comment
+print("Hello "+input("what is your name")+"!")
+
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
+
+numOfLetters = len("Angela")
+print(numOfLetters)
