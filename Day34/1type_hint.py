48d47d83d20b2454642b8f2ceb63c076729a485e
bayram <bayramguney@yahoo.com>
2024-07-16 16:09:02 -0400

    Day34  completed

---
+++ Day34/1type_hint.py
@@ -0,0 +1,10 @@
+
+
+def police_check(age: int):
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return
+
+police_check("twelve")   # gives error because type is not matched
